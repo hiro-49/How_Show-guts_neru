fba9a3adef1f8bad8a5826c463a9ebcd1aa368a8
hiro-49 <kazuki97101@gmail.com>
2019-11-07 22:56:07 +0900

    チュートリアル_読み込み完了後今日の日付と来年の1/1を入力する Squashed commit of the following:
    commit 47c3841f62f1dcdc4dee43a7bb27ea16a2158ddb
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:50:18 2019 +0900
    
        チュートリアル作成_針を動かす
    
    commit 055e04436fd964635f0a00e073ac392c218d58d4
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:46:35 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit 71091004c8c97e4ecda885ec9fb61eb0df9e7f4b
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:45:02 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit bdfa0951def435857b9d300aba42cef09262edb4
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:38:26 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit eb454eda63cb1f7f2aa447c65e51857075994f53
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:36:38 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit 6041ff264c2cabd4179f8562036552a570c02545
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:34:14 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit e5e85a8af04953093c8e08517fa4bc8be43e36bb
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:28:23 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit a7e6c6dc22e5fdf52c392db5eb3c771e25567965
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:25:15 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit c40950ddd74c6b7649d449ff7335855571ba94a3
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:24:24 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit 9ebd87804635dc849288325b69a356396c4ca2bf
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:03:27 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit 4cfea74175f0bcecc6ff4494056f9d2f90399454
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 22:02:06 2019 +0900
    
        チュートリアル作成_同期処理したい
    
    commit 7d189e689cc75892ced9f3bf36129bab933c8f66
    Author: hiro-49 <kazuki97101@gmail.com>
    Date:   Thu Nov 7 21:59:09 2019 +0900
    
        チュートリアル作成_同期処理したい

--- src/js/app.js
+++ src/js/app.js
@@ -12,26 +12,37 @@ import BottomNewsScroll from "./modules/BottomNewsScroll";
   //正規表現
   const regexp_date = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
   const regexp_days = new RegExp(/^\d+$/);
+  //変数
+  var today;
+  //タイプライター風表示用
+  var delay = 100;
   //インスタンス
   var c_dateCalc = new dateCalc();
   var needle = new Needle(document.getElementById('needle0'));
   var stringColor = new StringColor(document.getElementById('sideNewsSentence'));
   var buttomNewsScroll = new BottomNewsScroll(document.getElementById('bottomNewsSentence'));
 
+  //html読み込み後実行
+  window.onload = () => {
+    today = new Date();
+    tutorial()
+    .then(inputDate0)
+    .then(inputDate1);
+  }
 
   //イベントリスナーを追加
   i_date0.addEventListener('blur', ()=>{
     if(regexp_date.test(i_date0.value)){
-
+      needle.AnimToDate1();
     } else {
       i_date0.value = "false";
-      needle.AnimToDate1();
     }
 
   })
 
   i_date1.addEventListener('blur', ()=>{
     if(regexp_date.test(i_date1.value)){
+      needle.AnimToDays();
       s_days.value = c_dateCalc.CalcDate0_Date1(i_date0, i_date1);
     } else {
       i_date1.value = "false";
@@ -40,9 +51,68 @@ import BottomNewsScroll from "./modules/BottomNewsScroll";
 
   s_days.addEventListener('blur', ()=>{
     if(regexp_days.test(s_days.value)){
+      needle.AnimToDate1();
       i_date1.value = c_dateCalc.Date0_Days(i_date0, s_days);
     } else {
       s_days.value = "false";
     }
   })
+
+  s_days.addEventListener('focus', ()=>{
+    needle.AnimToDays();
+  })
+
+  //関数
+
+  //promise用関数
+  //tutorial処理
+  var tutorial = () => {
+    return new Promise((resolve, reject) => {
+      if(today != null){
+        resolve();
+      }
+    })
+  }
+
+  var inputDate0 = () => {
+    return new Promise((resolve, reject) => {
+      let str = today.getFullYear() + '-' + ('00' + (today.getMonth() + 1)).slice(-2) + '-' + ('00' + today.getDate()).slice(-2);
+      i_date0.focus();
+      setTypewriter(i_date0, str)
+      .then(() => {
+        i_date0.blur();
+        resolve();
+      })
+    })
+  }
+
+  var inputDate1 = () => {
+    let str = (today.getFullYear() + 1) + '-01-01';
+    i_date1.focus();
+    setTypewriter(i_date1, str)
+    .then(() => {
+      i_date1.blur();
+    })
+  }
+
+  //タイプライター風表示
+  var setTypewriter = (input, str) => {
+
+    return new Promise((resolve, reject) => {
+      let typewriter = setInterval(() => {
+        let buf = input.value;
+        let writed = buf.length;
+        let write = "";
+        if(writed < str.length){
+          write = str.charAt(writed);
+          input.value = buf + write;
+        }else{
+          console.log(input.value);
+          clearInterval(typewriter);
+          resolve();
+        }
+      }, delay);
+    })
+  }
+
 }
